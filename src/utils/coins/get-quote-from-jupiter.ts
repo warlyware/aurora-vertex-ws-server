8a9bcf9961e5d6f0e83226be694ddf1fe937a9f2
warly <warlyware@pm.me>
2023-12-23 02:22:04 -0800

    Change quote url

--- src/utils/coins/get-quote-from-jupiter.ts
+++ src/utils/coins/get-quote-from-jupiter.ts
@@ -15,9 +15,13 @@ export const getQuoteFromJupiter = async ({
 
   //quote-api.jup.ag/v6/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&amount=1000000&slippageBps=1
 
-  const quote = await fetch(
-    `https://quote-api.jup.ag/v6/quote?inputMint=${inputMint}&outputMint=${outputMint}&amount=${amount}&slippageBps=1`
-  );
+  const url = `https://quote-api.jup.ag/v6/quote?inputMint=${inputMint}&outputMint=${outputMint}&amount=${amount}&slippageBps=1`;
+
+  const quote = await fetch(url, {
+    method: "GET",
+  });
+
+  console.log({ url });
 
   const quoteJson = await quote.json();
 

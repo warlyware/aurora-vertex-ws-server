b6b4f1a4762c8d32146f7ee9f3394ddda7c4a97b
warly <warlyware@pm.me>
2025-02-19 15:44:47 -0800

    Fix cors

--- src/setup/index.ts
+++ src/setup/index.ts
@@ -35,7 +35,7 @@ export const setupApp = () => {
   app.use(express.urlencoded({ extended: true }));
 
   app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://auroravertex.click"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
